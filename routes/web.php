81f0d12a287fb96c83fd9369c19f3d5ae9aab92c
traqe <92789583+traqe@users.noreply.github.com>
2022-11-28 16:49:27 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,12 +29,11 @@ Route::get('/shop', function () {
     return view('shop.index');
 });
 
-
 // route to software-development-service-page
 Route::get('/software-development', function () {
     return view('services.software-dev');
+});
 
 Route::get('/sage', function () {
     return view('services.sage');
-
 });
