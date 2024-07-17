f6c8adc156957141f21e93672d70dd9e3a64a2d5
traqe <tnrwatida@gmail.com>
2024-07-17 12:00:34 +0200

    done with canopy service

--- resources/views/services/services.blade.php
+++ resources/views/services/services.blade.php
@@ -1,24 +1,24 @@
 @extends('header')
 @section('content')
-<div id="banner-area" class="banner-area" style="background-image:url(public/services/images/banner/banner4.jpg)">
-  <div class="banner-text">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="banner-heading">
-            <h1 style="color: rgb(64,64,180)" class="banner-title">Service</h1>
-            <nav aria-label="breadcrumb">
-              <ol class="breadcrumb justify-content-center">
-                <li class="breadcrumb-item"><a style="color: rgb(64,64,180)" href="/afc">Home</a></li>
-                <li class="breadcrumb-item"><a style="color: rgb(64,64,180)" href="#ts-service-area">Services</a></li>
-                <li class="breadcrumb-item active" aria-current="services">All Services</li>
-              </ol>
-            </nav>
-          </div>
-        </div><!-- Col end -->
-      </div><!-- Row end -->
-    </div><!-- Container end -->
-  </div><!-- Banner text end -->
+<div id="banner-area" class="banner-area" style="background-image:url(public/services/images/slider-main/canopy-slide.jpeg)">
+    <div class="banner-text">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="banner-heading">
+                        <h1 class="banner-title">SERVICES</h1>
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb justify-content-center">
+                                <li class="breadcrumb-item"><a href="/afc">Home</a></li>
+                                <li class="breadcrumb-item"><a href="services">Services</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">All Services</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div><!-- Col end -->
+            </div><!-- Row end -->
+        </div><!-- Container end -->
+    </div><!-- Banner text end -->
 </div><!-- Banner area end -->
 
 <section id="main-container" class="main-container pb-2">
